3b66920c12bd05ca48e63b2265d4bc5e2f37f175
Chinazo <chinazoonwukaike@gmail.com>
2025-04-24 13:17:56 -0500

    create permissions infrastructure, add permissions and connect to settings page

--- app/permissions/index.tsx
+++ app/permissions/index.tsx
@@ -1,10 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import React from "react";
+import { ThemedText } from "@/presentation/components/shared/ThemedText";
+import { usePermissionsStore } from "@/presentation/store/usePermissions";
+import { requestLocationPermission } from "@/core/actions/permissions/location";
+import ThemedPressable from "@/presentation/components/shared/ThemedPressable";
 
 const PermissionsScreen = () => {
+  const { locationStatus } = usePermissionsStore();
+
   return (
-    <View>
-      <Text>PermissionsScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ThemedPressable onPress={requestLocationPermission}>
+        Habilitar ubicación
+      </ThemedPressable>
+      <ThemedText>Estado actual: {locationStatus}</ThemedText>
     </View>
   );
 };
---
+++ presentation/providers/PermissionsCheckerProvider.tsx
@@ -0,0 +1,37 @@
+import { PropsWithChildren, useEffect } from "react";
+import { View, Text, AppState } from "react-native";
+import { usePermissionsStore } from "../store/usePermissions";
+import { PermissionStatus } from "@/infrastructure/interfaces/location";
+import { router } from "expo-router";
+
+const PermissionsCheckerProvider = ({ children }: PropsWithChildren) => {
+  const { locationStatus, checkLocationPermission } = usePermissionsStore();
+
+  useEffect(() => {
+    if (locationStatus === PermissionStatus.GRANTED) {
+      router.replace("/map");
+    } else if (locationStatus !== PermissionStatus.CHECKING) {
+      router.replace("/permissions");
+    }
+  }, [locationStatus]);
+
+  useEffect(() => {
+    checkLocationPermission();
+  }, []);
+
+  useEffect(() => {
+    const susbcription = AppState.addEventListener("change", (nextAppState) => {
+      if (nextAppState === "active") {
+        checkLocationPermission();
+      }
+    });
+
+    return () => {
+      susbcription.remove();
+    };
+  }, []);
+
+  return <>{children}</>;
+};
+
+export default PermissionsCheckerProvider;
